8cf6d793901f11b0430a02039bdfe5daa6837200
Jonathan Torres <jonathan@actycrea.com>
2023-07-24 18:09:16 +0200

    Presupuesto

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
+import Presupuesto from './components/Presupuesto.vue';
 
 </script>
 
 <template>
   <h1>Desde VUE</h1>
+  <div class="contenedor contenedor-header contenedor-sombra"></div>
+  <Presupuesto />
 </template>
 
 <style scoped>
---
+++ src/components/Presupuesto.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+<template>
+  <form>
+    <div class="campo">
+        <label for="">
+            Definir Presupuesto
+        </label>
+        <input type="text" id="nuevo-presupuesto" class="nuevo-presupuesto" placeholder="Definir Presupuesto">
+    </div>
+    <input type="submit" value="Definir Presupuesto">
+  </form>
+</template>
+<style>
+
+</style>
